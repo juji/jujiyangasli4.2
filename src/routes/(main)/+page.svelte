91a6e41b18e83cbe20bb2c8e1dc49cf6e6a05710
juji <jujiyangasli@gmail.com>
2023-11-16 20:03:55 +0700

    no pendulum

--- src/routes/(main)/+page.svelte
+++ src/routes/(main)/+page.svelte
@@ -1,14 +1,14 @@
 <script>
-  // import Hello from '$lib/pages/hello/index.svelte'
-  // import Contact from '$lib/pages/contact/index.svelte'
+  import Hello from '$lib/pages/hello/index.svelte'
+  import Contact from '$lib/pages/contact/index.svelte'
   import Techs from '$lib/pages/techs/index.svelte'
-  // import Works from '$lib/pages/works/index.svelte'
+  import Works from '$lib/pages/works/index.svelte'
 
   export let data;
 </script>
 
-<!-- <Hello /> -->
-<!-- <Works works={data.works} /> -->
+<Hello />
+<Works works={data.works} />
 <Techs />
-<!-- <Contact /> -->
+<Contact />
 
