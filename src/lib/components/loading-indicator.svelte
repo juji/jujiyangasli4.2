b34f6ea96f367c235d1861d933b9ca1e75a54cfa
juji <jujiyangasli@gmail.com>
2023-11-17 10:14:18 +0700

    loading indicator 2

--- src/lib/components/loading-indicator.svelte
+++ src/lib/components/loading-indicator.svelte
@@ -4,16 +4,28 @@
 
   let loading = false;
   let percent = '0%';
+  let cover = false;
+
+  let to: ReturnType<typeof setTimeout>;
 
   onNavigate((navigation) => {
     
     loading = true
-    percent = '60%'
+    percent = '72%'
 
     return new Promise(async (resolve) => {
       resolve()
+
+      if(to) clearTimeout(to)
+      to = setTimeout(() => {
+        cover = true
+      }, 1000)
+
       await navigation.complete;
+
       percent = '100%'
+      clearTimeout(to)
+      cover = false
       setTimeout(() => {
         loading = false
         percent = '0%'
@@ -27,6 +39,7 @@
 <div 
   class="loading-indicator"
   class:loading
+  class:cover
   style={`--loading-percent: ${percent}`}
 ></div>
 
@@ -40,25 +53,58 @@
   .loading-indicator{
 
     --loading-percent: 0%;
-
     position: fixed;
     top:0;
     left:0;
-    width: var(--loading-percent);
-    height: 2px;
-    background: rgb(0,97,196);
-    background: radial-gradient(circle, rgba(47,170,255,1) 0%, rgba(0,97,196,1) 50%);
-    opacity: 0;
-    transition: width 200ms ease-out, opacity 100ms ease-out;
+    width: 100%;
     z-index: 1000;
-    background-size: 200% auto;
-    background-position: 0 100%;
-    animation: gradient 500ms infinite;
-    animation-fill-mode: forwards;
-    animation-timing-function: linear;
+    opacity: 0;
+    transition: opacity 100ms ease-out;
+
+    &::before{
+      content: ' ';
+      position: fixed;
+      top:0;
+      left:0;
+      width: 0%;
+      height: 0%;
+      z-index: 999;
+      background-color: black;
+      opacity: 0;
+      transition: opacity 200ms ease-out;
+    }
+
+    &::after{
+      content: ' ';
+      position: fixed;
+      top:0;
+      left:0;
+      width: var(--loading-percent);
+      height: 2px;
+      background: rgb(0,97,196);
+      background: radial-gradient(circle, rgba(76,181,255,1) 0%, rgba(0,97,196,1) 37%);
+      transition: width 250ms ease-out;
+      z-index: 1000;
+      background-size: 200% auto;
+      background-position: 0 100%;
+    }
+
+    &.cover{
+      &::before{
+        width: 100%;
+        height: 100%;
+        opacity: 0.64;
+      }
+    }
 
     &.loading{
       opacity: 1;
+      &::after{
+        animation: gradient 500ms infinite ease-out;
+        animation-fill-mode: forwards;
+        animation-timing-function: linear;
+      }
     }
+
   }
 </style>
