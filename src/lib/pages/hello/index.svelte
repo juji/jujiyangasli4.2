59070dce46cd8d6fb93efeb3bfca68bb16be30e1
juji <jujiyangasli@gmail.com>
2023-11-18 23:03:36 +0700

    force push

--- src/lib/pages/hello/index.svelte
+++ src/lib/pages/hello/index.svelte
@@ -23,8 +23,6 @@
     )
   })
 
-  // $: console.log(scrollY)
-
   // using element?.clientHeight instead of window height
   // to prevent scroll glitch in mobile, 
   // where window height changes on scroll
